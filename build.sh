c76f8d80ce4b09ced5d4280e3d9311b9209f0f53
unknown <32420586+colin-anderson@users.noreply.github.com>
2018-02-03 22:34:26 +0000

    Try to get Travis to stop building for net462

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -ev
 dotnet restore ./serilog-sinks-syslog.sln --runtime netstandard2.0
 dotnet build ./src/Serilog.Sinks.Syslog/Serilog.Sinks.Syslog.csproj --runtime netstandard2.0 --configuration Release
 
+dotnet build ./test/Serilog.Sinks.Syslog.Tests/Serilog.Sinks.Syslog.Tests.csproj --framework netcoreapp2.0
+
+
 # dotnet-xunit is a CLI tool that can only be executed from in the test folder
 cd ./test/Serilog.Sinks.Syslog.Tests
 dotnet xunit -framework netcoreapp2.0
