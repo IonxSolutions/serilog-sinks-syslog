034e7283198efebf0035f57d6399a4c2687ea0b0
unknown <32420586+colin-anderson@users.noreply.github.com>
2018-02-03 22:25:25 +0000

    Use .NET SDK 2.1.4 for Travis builds

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -ev
 
 dotnet restore ./serilog-sinks-syslog.sln --runtime netstandard2.0
-dotnet build ./src/Serilog.Sinks.Syslog/Serilog.Sinks.Syslog.csproj  --runtime netstandard2.0 --configuration Release
+dotnet build ./src/Serilog.Sinks.Syslog/Serilog.Sinks.Syslog.csproj --runtime netstandard2.0 --configuration Release
 
 # dotnet-xunit is a CLI tool that can only be executed from in the test folder
 cd ./test/Serilog.Sinks.Syslog.Tests
