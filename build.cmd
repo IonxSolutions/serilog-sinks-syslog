a1117a20decd93d533c3cf366b1b716c3af2c197
colin-anderson <32420586+colin-anderson@users.noreply.github.com>
2019-05-03 10:34:23 +0100

    `dotnet xunit` deprecated, using `dotnet test`

--- build.cmd
+++ build.cmd
@@ -1,12 +1,6 @@
-set START_DIR=%cd%
-
 dotnet restore .\serilog-sinks-syslog.sln
 dotnet build .\src\Serilog.Sinks.Syslog\Serilog.Sinks.Syslog.csproj --configuration Release
 
-rem dotnet-xunit is a CLI tool that can only be executed from in the test folder
-cd .\test\Serilog.Sinks.Syslog.Tests
-dotnet xunit
-
-cd %START_DIR%
+dotnet test .\test\Serilog.Sinks.Syslog.Tests\Serilog.Sinks.Syslog.Tests.csproj
 
 dotnet pack .\src\Serilog.Sinks.Syslog -c Release
