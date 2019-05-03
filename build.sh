6fd9aa2b1b269b14a09b72892902738fb2cc5e00
colin-anderson <32420586+colin-anderson@users.noreply.github.com>
2019-05-03 12:00:31 +0100

    FIx broken build script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 
-dotnet restore ./serilog-sinks-syslog.sln --runtime netstandard2.0
-dotnet build ./src/Serilog.Sinks.Syslog/Serilog.Sinks.Syslog.csproj --runtime netstandard2.0 --configuration Release
+dotnet restore ./serilog-sinks-syslog.sln
+dotnet build ./src/Serilog.Sinks.Syslog/Serilog.Sinks.Syslog.csproj --framework netstandard2.0 --configuration Release
 
 dotnet test ./test/Serilog.Sinks.Syslog.Tests/Serilog.Sinks.Syslog.Tests.csproj --framework netcoreapp2.0
 
