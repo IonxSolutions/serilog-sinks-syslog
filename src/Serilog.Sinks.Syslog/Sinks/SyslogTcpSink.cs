b9a1a934421d41cf749cf7ecaf6eb94aeba3aa69
David Basarab <dbasarab617@gmail.com>
2019-11-17 10:55:37 -0500

    Checking if client socket is not null.  If it is null then IsConnected will throw a null exception and stop the syslog sinks

--- src/Serilog.Sinks.Syslog/Sinks/SyslogTcpSink.cs
+++ src/Serilog.Sinks.Syslog/Sinks/SyslogTcpSink.cs
@@ -1,5 +1,5 @@
 // Copyright 2018 Ionx Solutions (https://www.ionxsolutions.com)
-// Ionx Solutions licenses this file to you under the Apache License, 
+// Ionx Solutions licenses this file to you under the Apache License,
 // Version 2.0. You may obtain a copy of the License at
 // http://www.apache.org/licenses/LICENSE-2.0
 
@@ -158,7 +158,7 @@ namespace Serilog.Sinks.Syslog
 
         private bool IsConnected()
         {
-            if (this.client == null || !this.client.Connected)
+            if (this.client?.Client == null || !this.client.Connected)
                 return false;
 
             var socket = this.client.Client;
