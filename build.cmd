5740a8db3a9112bf732fce38c183f7246af82f69
colin-anderson <32420586+colin-anderson@users.noreply.github.com>
2020-11-20 13:38:52 +0000

    feat: enable Source Link

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 dotnet restore .\serilog-sinks-syslog.sln
-dotnet build .\src\Serilog.Sinks.Syslog\Serilog.Sinks.Syslog.csproj --configuration Release
+dotnet build .\src\Serilog.Sinks.Syslog\Serilog.Sinks.Syslog.csproj --configuration Release -p:EnableSourceLink=true
 
 dotnet test .\test\Serilog.Sinks.Syslog.Tests\Serilog.Sinks.Syslog.Tests.csproj
 
