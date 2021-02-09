25860f948410da0d86060603ea4cb10d3f8497ea
Colin Anderson <32420586+colin-anderson@users.noreply.github.com>
2021-02-09 20:27:57 +0000

    feat: release 2.0.2 (#17)

--- test/Serilog.Sinks.Syslog.Tests/TcpSyslogSinkTests.cs
+++ test/Serilog.Sinks.Syslog.Tests/TcpSyslogSinkTests.cs
@@ -6,7 +6,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.NetworkInformation;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
